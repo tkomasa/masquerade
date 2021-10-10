09dd819d3435adee4ea75713eec30732ae93bd9e
Tucker Komasa <tskomasa@gmail.com>
2021-10-10 12:45:36 -0700

    turn into funcs

--- entity_gen.py
+++ entity_gen.py
@@ -7,33 +7,38 @@ import time
 requested_amount = int(input("How many entities should be created? (int): "))
 complexity = 10
 
-# npc class with name, plus traits
-class Entity:
-    def __init__(self, name, aggression, morality, wanderlust, greed):
-        self.name = name
-        self.aggression = aggression
-        self.morality = morality
-        self.wanderlust = wanderlust
-        self.greed = greed
-        
-        # use pairing functions to create behavior digit (szudzik is slightly faster)
-        self.behavior = szudzik.pair(self.aggression, self.morality, self.wanderlust)
-        
-# start of timer
-start_time = time.time_ns()
+
+def generate_entities(num_entities, complexity):
+    # npc class with name, plus traits
+    class Entity:
+        def __init__(self, name, aggression, morality, wanderlust, greed):
+            self.name = name
+            self.aggression = aggression
+            self.morality = morality
+            self.wanderlust = wanderlust
+            self.greed = greed
+            
+            # use pairing functions to create behavior digit (szudzik is slightly faster)
+            self.behavior = szudzik.pair(self.aggression, self.morality, self.wanderlust)
+            
+    # start of timer
+    start_time = time.time_ns()
 
 
-# loop creation of NPC objects
-with open('entities.csv', 'w') as f:
-    i = 0
-    entity_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-    entity_writer.writerow(['Name', 'Behavior ID', 'Aggression', 'Morality', 'Wanderlust', 'Greed'])
-    while i < requested_amount:
-        entity = Entity(f"npc#{i}", random.randint(0, complexity), random.randint(0, complexity), random.randint(0, complexity), random.randint(0, complexity))
-        entity_writer.writerow([entity.name, entity.behavior, entity.aggression, entity.morality, entity.wanderlust, entity.greed])
-        i += 1
-        
-# end of timer
-stop_time = time.time_ns()
-elapsed_time = (int(stop_time - start_time)) / 1000000000
-print(f"Finished generation of {requested_amount} entities in {elapsed_time} seconds.")
+    # loop creation of NPC objects
+    with open('entities.csv', 'w') as f:
+        i = 0
+        entity_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+        entity_writer.writerow(['Name', 'Behavior ID', 'Aggression', 'Morality', 'Wanderlust', 'Greed'])
+        while i < requested_amount:
+            entity = Entity(f"npc#{i}", random.randint(0, complexity), random.randint(0, complexity), random.randint(0, complexity), random.randint(0, complexity))
+            entity_writer.writerow([entity.name, entity.behavior, entity.aggression, entity.morality, entity.wanderlust, entity.greed])
+            i += 1
+            
+    # end of timer
+    stop_time = time.time_ns()
+    elapsed_time = (int(stop_time - start_time)) / 1000000000
+    print(f"Finished generation of {requested_amount} entities in {elapsed_time} seconds.")
+    
+
+generate_entities(requested_amount, complexity)
--- job_gen.py
+++ job_gen.py
@@ -7,36 +7,40 @@ import time
 requested_amount = int(input("How many entities should be created? (int): "))
 complexity = 10
 
-# npc class with name, plus traits
-class Job:
-    def __init__(self, name, aggression, morality, distance):
-        self.name = name
-        self.aggression = aggression
-        self.morality = morality
-        self.distance = distance
-        self.reward = random.randint(100, 10000)
-        
-        # assign the human-understandable job type
-        self.job_type = ""
-        
-        # use pairing functions to create behavior digit (szudzik is slightly faster)
-        self.behavior = szudzik.pair(self.aggression, self.morality, self.distance)
-        
-# start of timer
-start_time = time.time_ns()
+
+def generate_jobs(num_jobs, complexity):
+    class Job:
+        def __init__(self, name, aggression, morality, distance):
+            self.name = name
+            self.aggression = aggression
+            self.morality = morality
+            self.distance = distance
+            self.reward = random.randint(100, 10000)
+            
+            # assign the human-understandable job type
+            self.job_type = ""
+            
+            # use pairing functions to create behavior digit (szudzik is slightly faster)
+            self.behavior = szudzik.pair(self.aggression, self.morality, self.distance)
+            
+    # start of timer
+    start_time = time.time_ns()
 
 
-# loop creation of NPC objects
-with open('jobs.csv', 'w') as f:
-    i = 0
-    job_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-    job_writer.writerow(['Name', 'Behavior ID', 'Aggression', 'Morality', 'Distance', 'Reward'])
-    while i < requested_amount:
-        entity = Job(f"job#{i}", random.randint(0, complexity), random.randint(0, complexity), random.randint(0, complexity))
-        job_writer.writerow([entity.name, entity.behavior, entity.aggression, entity.morality, entity.distance, entity.reward])
-        i += 1
-        
-# end of timer
-stop_time = time.time_ns()
-elapsed_time = (int(stop_time - start_time)) / 1000000000
-print(f"Finished generation of {requested_amount} entities in {elapsed_time} seconds.")
+    # loop creation of NPC objects
+    with open('jobs.csv', 'w') as f:
+        i = 0
+        job_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+        job_writer.writerow(['Name', 'Behavior ID', 'Aggression', 'Morality', 'Distance', 'Reward'])
+        while i < num_jobs:
+            entity = Job(f"job#{i}", random.randint(0, complexity), random.randint(0, complexity), random.randint(0, complexity))
+            job_writer.writerow([entity.name, entity.behavior, entity.aggression, entity.morality, entity.distance, entity.reward])
+            i += 1
+            
+    # end of timer
+    stop_time = time.time_ns()
+    elapsed_time = (int(stop_time - start_time)) / 1000000000
+    print(f"Finished generation of {requested_amount} entities in {elapsed_time} seconds.")
+    
+
+generate_jobs(requested_amount, complexity)
