f0aea0abc512067f0b33e1f50e2a38183a5273f2
Tucker Komasa <tskomasa@gmail.com>
2021-10-10 10:18:08 -0700

    begin job generation

---
+++ job_gen.py
@@ -0,0 +1,38 @@
+# this is proof of concept I realize the method is dogshit dont bother me about it, I just needed the datasets for now to see if it will even work
+import random
+import csv
+from pairing_functions import szudzik
+import time
+
+requested_amount = int(input("How many entities should be created? (int): "))
+complexity = 10
+
+# npc class with name, plus traits
+class Job:
+    def __init__(self, name, aggression, morality, distance):
+        self.name = name
+        self.aggression = aggression
+        self.morality = morality
+        self.distance = distance
+        
+        # use pairing functions to create behavior digit (szudzik is slightly faster)
+        self.behavior = szudzik.pair(self.aggression, self.morality, self.distance)
+        
+# start of timer
+start_time = time.time_ns()
+
+
+# loop creation of NPC objects
+with open('jobs.csv', 'w') as f:
+    i = 0
+    job_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+    job_writer.writerow(['Name', 'Behavior ID', 'Aggression', 'Morality', 'Distance'])
+    while i < requested_amount:
+        entity = Job(f"job#{i}", random.randint(0, complexity), random.randint(0, complexity), random.randint(0, 1000))
+        job_writer.writerow([entity.name, entity.behavior, entity.aggression, entity.morality, entity.distance])
+        i += 1
+        
+# end of timer
+stop_time = time.time_ns()
+elapsed_time = (int(stop_time - start_time)) / 1000000000
+print(f"Finished generation of {requested_amount} entities in {elapsed_time} seconds.")
